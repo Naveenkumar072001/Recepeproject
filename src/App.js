d9414be0674d34cbf33711a3d0a00ca54d0752fb
Naveen <naveenkumarn072001@gmail.com>
2023-07-05 16:22:21 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react"
+import Recipe from './receipe';
+import { Levelcontext } from './context';
 
 function App(){
-  const[search,setsearch]=useState(0)
+  const[search,setsearch]=useState("")
+
+  const updatesearch= e =>{
+setsearch(e.target.value)
+  }
+  console.log(search)
 
   return(
-    <div class = "container">
-    <div class = "meal-wrapper">
-      <div class = "meal-search">
-        <h2 class = "title">Find Meals For Your Ingredients</h2>
+    
+    
+    <div className = "container">
+    <div className = "meal-wrapper">
+      <div className = "meal-search">
+        <h2 className = "title">Find Meals For Your Ingredients</h2>
         <blockquote>Real food doesn't have ingredients, real food is ingredients.<br/>
           <cite>- Jamie Oliver</cite>
         </blockquote>
-
-        <div class = "meal-search-box">
-          <input type = "text" class = "search-control" placeholder="Enter an ingredient "id = "search-input"/>
-          <button type = "submit" class = "search-btn btn" id = "search-btn">
-            <i class = "fas fa-search"></i>
+<form>
+        <div className = "meal-search-box">
+          <input type = "text" className = "search-control" onChange={updatesearch} placeholder="Enter an ingredient "id = "search-input"/>
+          <button type = "submit" className = "search-btn btn" id = "search-btn">
+            <i className = "fas fa-search"></i>
           </button>
         </div>
+        </form>
+        
       </div>
     </div>
     </div>
+    
+    
   )
 }
 export default App
--- src/receipe.js
+++ src/receipe.js
@@ -1,1 +1,13 @@
+import { useContext } from "react"
 
+function Recipe(){
+    const[setsearch]=useContext
+
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Recipe
